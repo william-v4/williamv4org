963ff1ee8f7df16abc951d0742fc4ce105c3258e
William Zhang <william.exe@posteo.net>
2022-05-09 21:19:43 -0600

    I had to delete the repo for git to work again

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById("logo").onclick() = function() {
+    document.getElementById("dropdowncontents").innerHTML = '<img src="williamv4net.png" style="width: 4%;" >';
+}
+var billboard = document.getElementById("billboard")
+var mood = Math.random(0,5)
+if (mood = 0) {
+    billboard.innerHTML = '<div class="level"><div class="level-item"><h1>OPEN</h1></div><div class="level-item"><h1>SUS</h1></div><div class="level-item"><h1>dynamic</h1></div><div class="level-item"><h1>COMMUNITY</h1></div>'
+}
+if (mood = 1) {
+    billboard.innerHTML = '<div class="level"><div class="level-item"><h1 style="color: 005BBC;">STAY</h1><br><h1 style="color: ffd700;">STRONG</h1></div><div class="level-item"><h1>Glory to Ukraine</h1></div></div>'
+}
+if (mood = 2) {
+    billboard.innerHTML = '<h1>very SUS indeed</h1>'
+}
+if (mood = 3) {
+    billboard.innerHTML = '<div class="level"><div class="level-item"><img src="amogus.webp"></div><div class="level-item"><h1 style="font-size: 64pt;">></h1></div><div class="level-item"><img src="amogusingame"></div></div><br><a href="./amogusisbetter">learn why</a>'
+}
+if (mood = 4) {
+    
+}
